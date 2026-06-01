ae3ab3c09cd717fd20575ecd005bb63eb5c85649
Shashank-raj1907 <shashankraj2116193@gmail.com>
2026-06-01 14:32:15 +0530

    added edit button functionality

--- src/Lists.jsx
+++ src/Lists.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 function Lists({items, setItems}) {
 
+  const [editingId, setEditingId] = useState(null);
+  const [editedText, setEditedText] = useState("");
+
   const toggleCheckbox = (id) => {
     setItems(
       prevItems => prevItems.map(item => item.id === id ? {...item, completed: !item.completed} : item)
@@ -10,6 +13,14 @@ function Lists({items, setItems}) {
   const deleteItem = (id) => {setItems(
     prevItems => prevItems.filter(item => item.id !== id)
   )};
+
+  const saveEdit = (id) => {
+    setItems(
+      prevItems => prevItems.map(item => item.id === id ? {...item, text: editedText} : item)
+    );
+    setEditingId(null);
+    setEditedText("");
+  }
 
   return (
     <div className="list_container">
@@ -22,9 +33,34 @@ function Lists({items, setItems}) {
             }}
             key={item.id}>
           <input type="checkbox" checked={item.completed} onChange={() => toggleCheckbox(item.id)}/>
-          <span className="task_title" style={{ textDecoration: item.completed ? "line-through" : "none" }}>
-            {item.text}
-          </span>
+          {
+            editingId == item.id ? (
+              <input
+                class = "task_edit_input"
+                value={editedText}
+                onChange={(e) => setEditedText(e.target.value)}
+                // onBlur={() => saveEdit(item.id)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") saveEdit(item.id);
+                  if (e.key === "Escape") setEditingId(null);
+                }}
+                autoFocus
+              />
+            ) : (
+              <span className="task_title" style={{ textDecoration: item.completed ? "line-through" : "none" }}>
+                {item.text}
+              </span>
+            )
+          }
+
+          {
+            editingId == item.id ? (
+              <button className="task_save" onClick={() => saveEdit(item.id)}>Save</button>
+            ) : (
+              <button className="task_edit" onClick={() => {setEditingId(item.id); setEditedText(item.text);}}>Edit</button>
+            )
+          }
+          
           <button className="task_delete" onClick={() => deleteItem(item.id)}>Delete</button>
         </li>
       ))}
